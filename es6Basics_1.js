4af13352fe7dc444e4006223356a59f2f9d3670d
gergokutu <gergo.kutu@gmail.com>
2020-11-15 16:35:00 +0100

    Apply let keyword

--- es6Basics_1.js
+++ es6Basics_1.js
@@ -1,1 +1,26 @@
-console.log("TEST");
+let catName;
+let quote;
+
+function catTalk() {
+  "use strict";
+
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+
+}
+catTalk();
+
+console.log("catName:", catName);
+console.log("quote:", quote);
+
+function checkScope() {
+  'use strict';
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);
+  }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+console.log("Return value:", checkScope());
