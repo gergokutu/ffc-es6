4aeb80c72f24a0e66b0a8ea22e59e594ed3404b8
gergokutu <gergo.kutu@gmail.com>
2020-11-15 17:20:36 +0100

    Copy array with spread operator

--- es6Basics_1.js
+++ es6Basics_1.js
@@ -82,3 +82,8 @@ const sum = (...args) => {
   return args.reduce((a, b) => a + b, 0);
 }
 console.log("Sum:", sum(1, 2, 3));
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+arr2 = [...arr1];
+console.log(arr2);
