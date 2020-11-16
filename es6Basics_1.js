c2e70ae27185e6000971bb3c8a78d975d7202b4a
gergokutu <gergo.kutu@gmail.com>
2020-11-16 10:27:41 +0100

    Handle nested objects with destructuring

--- es6Basics_1.js
+++ es6Basics_1.js
@@ -104,3 +104,11 @@ const { today, tomorrow } = HIGH_TEMPERATURES;
   const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
 }
 
+const LOCAL_FORECAST = {
+  yesterday: { low: 61, high: 75 },
+  today: { low: 64, high: 77 },
+  tomorrow: { low: 68, high: 80 }
+};
+const { today: { low: lowToday, high: highToday } } = LOCAL_FORECAST;
+console.log("Today high is:", highToday, "degrees.");
+
