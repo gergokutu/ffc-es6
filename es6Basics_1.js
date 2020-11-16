293cf42d5e3b170dcd0bcfb7399ddaf51c45fb99
gergokutu <gergo.kutu@gmail.com>
2020-11-16 10:42:41 +0100

    Array destructuring with rest operator

--- es6Basics_1.js
+++ es6Basics_1.js
@@ -130,3 +130,19 @@ console.log("Today high is:", highToday, "degrees.");
   [a, b] = [b, a];
   console.log(`B: ${b} and A: ${a}`);
 }
+
+{
+  // The result is similar to Array.prototype.slice()
+  const [a, b, ...arr] = [1, 2, 3, 4, 5, 7];
+  console.log(a, b); // 1, 2
+  console.log(arr); // [3, 4, 5, 7]
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+  "use strict";
+  const [a, b, ...arr] = list;
+  return arr;
+}
+const arr = removeFirstTwo(source);
+console.log(`Source: ${source}, removeFirstTwo: ${arr}`);
