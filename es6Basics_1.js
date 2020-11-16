28f941a6a3a25847a137dd0ab5fd67685317f746
gergokutu <gergo.kutu@gmail.com>
2020-11-16 10:19:14 +0100

    Destructuring from Objects 2

--- es6Basics_1.js
+++ es6Basics_1.js
@@ -94,3 +94,11 @@ const HIGH_TEMPERATURES = {
   tomorrow: 80
 };
 const { today, tomorrow } = HIGH_TEMPERATURES;
+
+const HIGH_TEMPERATURES = {
+  yesterday: 75,
+  today: 77,
+  tomorrow: 80
+};
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+
