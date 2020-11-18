1fd4804499ff6d2ae6ef3f8fe59acf22d9156f40
gergokutu <gergo.kutu@gmail.com>
2020-11-18 10:19:42 +0100

    Class syntax, getter/setter

--- es6Basics_2.js
+++ es6Basics_2.js
@@ -20,3 +20,25 @@ class Vegetable {
 }
 const carrot = new Vegetable('carrot');
 console.log(carrot.name);
+
+class Thermostat {
+  constructor(temperature) {
+    this._temperature = temperature;
+  }
+
+  get temperature() {
+    return 5 / 9 * (this._temperature - 32);
+  }
+
+  set temperature(tempInCelsius) {
+    this._temperature = tempInCelsius * 9.0 / 5 + 32;
+  }
+}
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+console.log(temp);
+
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
+console.log(temp);
