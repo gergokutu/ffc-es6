eb2d2ae2b231659271c7f3ca3075a5ae589c7a86
gergokutu <gergo.kutu@gmail.com>
2020-11-18 09:59:12 +0100

    Class syntax, constructor

--- es6Basics_2.js
+++ es6Basics_2.js
@@ -7,3 +7,16 @@ const bicycle = {
 };
 bicycle.setGear(3);
 console.log(bicycle.gear);
+
+// Old syntax
+// var Vegetable = function(name) {
+//   this.name = name;
+// }
+
+class Vegetable {
+  constructor(name) {
+    this.name = name;
+  }
+}
+const carrot = new Vegetable('carrot');
+console.log(carrot.name);
