611935c654b19c47f371965689fa9ebd987e1c69
gergokutu <gergo.kutu@gmail.com>
2020-11-18 11:01:17 +0100

    Import default export

--- import.js
+++ import.js
@@ -4,7 +4,14 @@ import { uppercaseString, lowercaseString } from './string_functions.js';
 console.log(uppercaseString("hello"));
 console.log(lowercaseString("WORLD!"));
 
+// Named > all with *
 import * as stringFunctions from "./string_functions.js";
 
 stringFunctions.uppercaseString("hello");
 stringFunctions.lowercaseString("WORLD!");
+
+// Import default export
+// You can give any name instead of subtract
+import subtract from "./math_functions.js";
+
+subtract(7, 4);
